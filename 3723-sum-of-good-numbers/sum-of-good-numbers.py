2c6959197a20f0df3a2df58ef26e322a06916ae1
Amokaasaravanan <amokaass@gmail.com>
2026-07-02 14:12:34 +0530

    Time: 3 ms (54.04%) | Memory: 19.2 MB (92.17%) - LeetSync

---
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sumOfGoodNumbers(self, nums, k):
+        total = 0
+        n = len(nums)
+
+        for i in range(n):
+            good = True
+
+            if i - k >= 0 and nums[i] <= nums[i - k]:
+                good = False
+
+            if i + k < n and nums[i] <= nums[i + k]:
+                good = False
+
+            if good:
+                total += nums[i]
+
+        return total
