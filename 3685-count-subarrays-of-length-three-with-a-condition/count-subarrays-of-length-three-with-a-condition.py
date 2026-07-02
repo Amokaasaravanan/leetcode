d9c519e3be7c7566ef067cde7734d596f4c689cf
Amokaasaravanan <amokaass@gmail.com>
2026-07-02 09:55:45 +0530

    Time: 7 ms (60.27%) | Memory: 19.4 MB (4.57%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.py
@@ -4,6 +4,5 @@ class Solution:
 
         for i in range(len(nums) - 2):
             if 2 * (nums[i] + nums[i + 2]) == nums[i + 1]:
-                count += 1
-
+               count+=1
         return count
